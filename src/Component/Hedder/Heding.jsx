f86bf6f81d9525a5e55d8a30473e4956ff94b671
anudaransara <90440430+anudaransara@users.noreply.github.com>
2024-10-13 11:59:09 +0530

    new branch update

--- src/Component/Hedder/Heding.jsx
+++ src/Component/Hedder/Heding.jsx
@@ -9,10 +9,14 @@ import { Link } from "react-router-dom";
 function Heding() {
   return (
     <div className='container'>
+      <h3>new git branch</h3>
         <Navbar className='mt-10 pt-10'>           
             <Navbar.Brand>
               <Link to='/'><FaHospitalAlt className='logo-Icon'/></Link>
             </Navbar.Brand>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
             <Nav className="me-auto">
                 <Nav.Link ><Link to ='/' className='link'>Home</Link></Nav.Link>
                 <Nav.Link ><Link to ='/doctor'>All Doctor</Link></Nav.Link>
